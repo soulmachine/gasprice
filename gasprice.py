0efb4582dae4fad7eb50cbc3fa8f7abaedfafb6e
soulmachine <soulmachine@gmail.com>
2021-10-19 14:46:44 -0700

    Initial commit

---
+++ gasprice.py
@@ -0,0 +1,37 @@
+import json
+import os
+import time
+from datetime import datetime, timezone
+
+import requests
+
+headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+}
+
+
+def get_gas_price():
+    try:
+        response = requests.get('https://etherchain.org/api/gasnow',
+                                headers=headers)
+        obj = response.json()
+        if obj['code'] == 200:
+            with open(
+                    os.path.join(
+                        'data',
+                        datetime.now(timezone.utc).strftime("%Y-%m") + ".json"),
+                    "a") as f:
+                f.write(json.dumps(obj['data']) + '\n')
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    while True:
+        get_gas_price()
+        # Run for 30 minutes - 5 seconds
+        if time.time() - start_time > 30 * 60 - 5:
+            break
+        time.sleep(5)  # sleep for 5 seconds
